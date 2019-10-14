31535293780e4b06cef4f6d48719e953c02ba41a
yeji <yjll1019@naver.com>
2019-10-14 22:00:53 +0900

    feat: Ladder 객체 생성

---
+++ src/main/java/ladder/Ladder.java
@@ -0,0 +1,30 @@
+package ladder;
+
+import java.util.List;
+
+import ladder.exception.LadderHeightException;
+
+public final class Ladder {
+    private final List<Lines> ladder;
+
+    public Ladder(final List<Lines> ladder) {
+        validateLadderHeight(ladder);
+        this.ladder = ladder;
+    }
+
+    private void validateLadderHeight(List<Lines> ladder) {
+        if (ladder.size() <= 0) {
+            throw new LadderHeightException();
+        }
+    }
+
+    public int climb(int position) {
+        int currentPosition = position;
+
+        for (Lines lines : ladder) {
+            currentPosition = lines.move(currentPosition);
+        }
+
+        return currentPosition;
+    }
+}
