25595b1c4c846c477a5ef2fd1a4a021192ad5eab
yeji <yjll1019@naver.com>
2019-10-15 19:47:07 +0900

    refactor: InputView 상수 추출

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -5,16 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    public static final String USER_INPUT_SEPARATOR = ",";
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static List<String> getUserNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return Arrays.asList(SCANNER.nextLine().split(","));
+        return Arrays.asList(SCANNER.nextLine().split(USER_INPUT_SEPARATOR));
     }
 
     public static List<String> getRewards() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        return Arrays.asList(SCANNER.nextLine().split(","));
+        return Arrays.asList(SCANNER.nextLine().split(USER_INPUT_SEPARATOR));
     }
 
     public static int getLadderHeight() {
